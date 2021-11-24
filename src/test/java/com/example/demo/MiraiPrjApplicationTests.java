68f4f595cc2d7fcd57bb3694ad126b2a8abc60ae
UU144359 <UU144359@YA3751.ancor2.r.recruit.co.jp>
2021-11-24 21:44:04 +0900

    junitTest作成

--- src/test/java/com/example/demo/MiraiPrjApplicationTests.java
+++ src/test/java/com/example/demo/MiraiPrjApplicationTests.java
@@ -1,11 +1,8 @@
 package com.example.demo;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 class MiraiPrjApplicationTests {
 
@@ -17,15 +14,5 @@ class MiraiPrjApplicationTests {
 	public void applicationContextTest() {
 		//MiraiPrjApplication.main(new String[] {});
 	}
-	
-	
-	
 
-	
-
-
-	
-	
-	
-	
 }
