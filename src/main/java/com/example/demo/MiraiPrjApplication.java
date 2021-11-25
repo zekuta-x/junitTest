81da25d389308f754e5c0052a5354225599794c5
UU144359 <UU144359@YA3751.ancor2.r.recruit.co.jp>
2021-11-25 17:47:55 +0900

    jacoco除外対応

--- src/main/java/com/example/demo/MiraiPrjApplication.java
+++ src/main/java/com/example/demo/MiraiPrjApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@lombok.Generated
 @SpringBootApplication
 public class MiraiPrjApplication {
 	
