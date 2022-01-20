14613f60452673bfbe660656bdd229ca22427954
Toriyama Hidetaka <mgslove11zekuta@gmail.com>
2022-01-20 15:49:08 +0900

    更新

--- src/main/java/com/example/demo/JudgeUtils.java
+++ src/main/java/com/example/demo/JudgeUtils.java
@@ -6,7 +6,7 @@ import java.util.Date;
 public class JudgeUtils extends MiraiPrjApplication {
 
 	// 定数
-	public static final int AGE = 17;
+	public static final int AGE = 18;
 
 	/**
 	 * 課金対象か判定するメソッド<br>
